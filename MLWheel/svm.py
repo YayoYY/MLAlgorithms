3af9d1858a80727e4012ff3de63faf371ce41768
YayoYY <346756297@qq.com>
2019-01-23 21:05:03 +0800

    19.1.23

--- MLWheel/svm.py
+++ MLWheel/svm.py
@@ -1,9 +1,58 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
 
+'''
+
+Perceptron with SGD method
+
+1. import "svm" from "MLWheel" module
+
+	>>> from MLWheel import svm
+
+2. initialize a Perceptron model
+
+	>>> clf = svm.SVM(**params)
+
+    features : Features' name.
+
+    C: Penalty parameter. Decrease C can avoid overfitting.
+
+    kernel: kernel function type, ['linear', 'rbf', 'poly', 'sigmoid'], default value: 'linear'
+    
+    max_iter: Max iteration num, default value: 100. Decrease max_iter can avoid overfitting.
+    
+    degree: Parameter for kernel functions.
+
+    gamma: Parameter for kernel functions.
+
+    coef0: Parameter for kernel functions.
+
+3. method
+    
+    (1) use your data fit a model
+
+    	>>> clf.fit(X, y)
+
+        X is an array of shape (n_samples, n_features)
+        y is an array of shape (n_samples,) ps. -1 for n-sample, +1 for p-sample
+        return type self (an Perceptron instance)
+
+    (2) predict
+
+        >>> clf.predict(X)
+
+        X is an array of shape (n_samples, n_features)
+        return type is an array of shape (n_samples,)
+
+4. attributes
+
+    
+
+'''
+
 __author__ = 'YayoYY'
 
-import numpy as numpy
+import numpy as np
 
 class SVM():
 
@@ -27,7 +76,7 @@ class SVM():
 
 	@property
 	def support_(self):
-		return np.where(self.__alpha != 0)[0]
+		return np.where(self.__alpha[self.__alpha == self.__C])[0]
 
 	@property
 	def n_support_(self):
@@ -35,11 +84,16 @@ class SVM():
 
 	@property
 	def support_vectors_(self):
-		return self.__X[[self.support_]]
+		return self.__X[self.support_,:]
 
 	@property
-	def get_params(self):
+	def get_params_(self):
 		return self.__params
+
+	@property
+	def iter_num_(self):
+		return self.__iter_num
+	
 
 	def fit(self, X, y):
 		self.__X = X.T
@@ -54,8 +108,9 @@ class SVM():
 		return np.sign(self.__calc_g(Gram))
 
 	def __smo(self):
+		self.__iter_num = 0
 		for i in range(self.__max_iter):
-			g = self.__calc_g(self.__X)
+			g = self.__calc_g(self.__Gram)
 			yg = self.__y * g
 			voi_1 = np.logical_and(self.__alpha == 0, yg < 1)
 			voi_2 = np.logical_and(np.logical_and(self.__alpha > 0, self.__alpha < self.__C), yg != 1)
@@ -76,13 +131,34 @@ class SVM():
 				alpha_2 = np.random.choice(np.where(E == min(E))[0])
 			else:
 				alpha_2 = np.random.choice(np.where(E == max(E))[0])
-			
+			eta = self.__Gram[alpha_1, alpha_1] + self.__Gram[alpha_2, alpha_2] - 2*self.__Gram[alpha_1, alpha_2]
+			alpha_2_new = self.__alpha[alpha_2] + self.__y[alpha_2]*(E[alpha_1] - E[alpha_2])/eta
+			if self.__y[alpha_1] == self.__y[alpha_2]:
+				L = max([1, self.__alpha[alpha_1]+self.__alpha[alpha_2]-self.__C])
+				H = min([self.__alpha[alpha_1]+self.__alpha[alpha_2], self.__C])
+			else:
+				L = max([0, self.__alpha[alpha_2]-self.__alpha[alpha_1]])
+				H = min([self.__C, self.__C-self.__alpha[alpha_2]+self.__alpha[alpha_1]])
+			alpha_2_old = self.__alpha[alpha_2]
+			alpha_2_new = np.array([L, alpha_2_new, H])[np.array([alpha_2_new<L, alpha_2_new>=L and alpha_2_new <=H, alpha_2_new>H])][0]
+			alpha_1_old = self.__alpha[alpha_1]
+			alpha_1_new = self.__alpha[alpha_1] + self.__y[alpha_1]*self.__y[alpha_2]*(alpha_2_old-alpha_2_new)
+			self.__alpha[alpha_1] = alpha_1_new
+			self.__alpha[alpha_2] = alpha_2_new
+			b1_new = -E[alpha_1] - self.__y[alpha_1]*self.__Gram[alpha_1,alpha_1]*(alpha_1_new-alpha_1_old)-self.__y[alpha_2]*self.__Gram[alpha_2,alpha_1]*(alpha_2_new-alpha_2_old)+self.__b
+			b2_new = -E[alpha_2] - self.__y[alpha_2]*self.__Gram[alpha_2,alpha_2]*(alpha_2_new-alpha_2_old)-self.__y[alpha_1]*self.__Gram[alpha_1,alpha_2]*(alpha_1_new-alpha_1_old)+self.__b
+			if b1_new  == b2_new:
+				self.__b = b1_new
+			else:
+				self.__b = (b1_new + b2_new)/2
+			self.__iter_num = self.__iter_num + 1
+		self.__coef = np.dot((self.__alpha * self.__y).reshape(1, len(self.__y)), self.__X.T).reshape((self.__X.shape[0]))
 
 	def __calc_E(self):
 		return self.__calc_g(self.__Gram) - self.__y
 
 	def __calc_g(self, Gram):
-		return (np.dot((self.__alpha * self.__y).reshape(1, len(self.__y)), Gram) + self.__b).reshape((len(self.__y),))
+		return (np.dot((self.__alpha * self.__y).reshape(1, len(self.__y)), Gram) + self.__b).reshape((Gram.shape[1],))
 
 	def __calc_Gram(self, X1, X2):
 		if self.__kernel == 'rbf':
